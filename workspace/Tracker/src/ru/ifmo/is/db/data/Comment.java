b8bcea884243ba3d96e6d360477c4da374d6fad7
aekuzmin <mrakys01@gmail.com>
2016-02-16 22:31:42 +0300

    Update Comment.java

--- workspace/Tracker/src/ru/ifmo/is/db/data/Comment.java
+++ workspace/Tracker/src/ru/ifmo/is/db/data/Comment.java
@@ -5,6 +5,7 @@ import ru.ifmo.is.db.DataClass;
 
 public class Comment extends DataClass {
 	public int id;
+	public int issueId;
 	public String author;
 	public String authorDisplay;
 	public Date dateCreated;
@@ -12,12 +13,14 @@ public class Comment extends DataClass {
 	
 	public Comment(
 			int id,
+			int issueId,
 			String author,
 			String authorDisplay,
 			Date dateCreated,
 			String text)
 	{
 		this.id = id;
+		this.issueId = issueId;
 		this.author = author;
 		this.authorDisplay = authorDisplay;
 		this.dateCreated = dateCreated;
