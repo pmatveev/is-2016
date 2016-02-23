f8836aab6541272c25ee764fea8da3c4c2bc7c14
pmatveev <philipp.matveev@gmail.com>
2016-02-24 00:02:19 +0300

    #20 additional class

---
+++ workspace/Tracker/src/ru/ifmo/is/util/Util.java
@@ -0,0 +1,28 @@
+package ru.ifmo.is.util;
+
+public class Util {
+	public static String nvl(String a) {
+		return a == null ? "" : a;
+	}
+	
+	public static String replaceHTML(String s) {
+		if (s == null) {
+			return null;
+		}
+		return s.replace("<", "&lt").replace(">", "&gt");
+	}
+	
+	public static String replaceStr(String s) {
+		if (s == null) {
+			return null;
+		}
+		return s.replace("\"", "\\\"");
+	}	
+	
+	public static String replaceStr1(String s) {
+		if (s == null) {
+			return null;
+		}
+		return s.replace("'", "\'");
+	}
+}
